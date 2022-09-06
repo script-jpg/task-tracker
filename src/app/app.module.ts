d06f8a06f3aef53b0ca46cb827f03df59de08e23
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2022-09-06 16:25:58 -0400

    Initalize Tasks, TaskItem, Button, Header, AddTask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskItemComponent,
+    ButtonComponent,
+    HeaderComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule
