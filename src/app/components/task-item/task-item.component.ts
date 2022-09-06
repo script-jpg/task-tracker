825c34831f60c5918ee073859f919a58850bd9a1
Nicholas Jiang <61169805+script-jpg@users.noreply.github.com>
2022-09-06 18:03:30 -0400

    Add toggle and delete to task-item

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,7 @@
 import { NONE_TYPE } from '@angular/compiler';
 import { Component, OnInit, Input } from '@angular/core';
 import {Task} from '../../task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -8,11 +9,22 @@ import {Task} from '../../task';
 })
 export class TaskItemComponent implements OnInit {
 
+  faTimes = faTimes;
   @Input() task: Task;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onToggleReminder(task: Task) {
+    console.log("onToggleReminder: task.reminder = " + task.reminder);
+  }
+
+  onDelete(task: Task) {
+    console.log("onDelete: task.id = " + task.id);
+  }
+
+
 
 }
